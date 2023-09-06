39206ecf4d90674ed0dbdba24c00f24898473b3d
HimanshuNegi100 <himanxunegi@gmail.com>
2023-09-06 12:27:25 +0530

    Add images folder to public

--- src/index.js
+++ src/index.js
@@ -15,14 +15,16 @@ const BookList = () => {
 };
 
 const Book = () => {
+  const title = "Atomic Habits";
+  const author = "James Clear";
   return (
     <article className="book">
       <img
         src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL900_SR900,600_.jpg"
         alt="Atomic Habits"
       />
-      <h2>Atomic Habits</h2>
-      <h2>James Clear</h2>
+      <h2>{title}</h2>
+      <h2>{author}</h2>
     </article>
   );
 };
