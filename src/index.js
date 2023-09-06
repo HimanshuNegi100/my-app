b6826f3b82da6217ca10f6c768989e5c93f56bed
HimanshuNegi100 <himanxunegi@gmail.com>
2023-09-06 12:32:16 +0530

    Use variables instead of hard-coded values

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const title = "Atomic Habits";
+const author = "James Clear";
+const image = "./images/book-1.jpg";
 const BookList = () => {
   return (
     <section className="bookList">
@@ -15,14 +18,9 @@ const BookList = () => {
 };
 
 const Book = () => {
-  const title = "Atomic Habits";
-  const author = "James Clear";
   return (
     <article className="book">
-      <img
-        src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL900_SR900,600_.jpg"
-        alt="Atomic Habits"
-      />
+      <img src={image} alt={title} />
       <h2>{title}</h2>
       <h2>{author}</h2>
     </article>
