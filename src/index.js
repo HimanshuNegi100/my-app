441d882473ac888828d6e9abaade47748ca8d2d5
HimanshuNegi100 <himanxunegi@gmail.com>
2023-09-06 12:49:28 +0530

    Use props instead of variables directly

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const title = "Atomic Habits";
-const author = "James Clear";
-const image = "./images/book-1.jpg";
+const firstBook = {
+  title: "Atomic Habits",
+  author: "James Clear",
+  img: "./images/book-1.jpg",
+};
+const secondBook = {
+  title: "The Psychology Of Money",
+  author: "Morgan Housel",
+  img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL900_SR900,600_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <h2>{author}</h2>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h2>{props.author}</h2>
     </article>
   );
 };
